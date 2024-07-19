c750e2dc9a3de66823a2bb796f6ef18dbb74fcb8
Jesus-JF <jjulcarimaf@uni.pe>
2024-07-19 15:50:06 -0500

    Initial Commit

---
+++ APIImages/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIImages
+//
+//  Created by Jhonatan Chavez on 19/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
